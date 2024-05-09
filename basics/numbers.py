817ae515a2086fdd4f755fc165b9dfb598d8e956
Praphul <praphulkumar2023@gmail.com>
2024-05-09 17:46:30 +0530

    Learning numbers - 01

---
+++ basics/numbers.py
@@ -0,0 +1,44 @@
+# Learning about numbers in python
+
+"""
+    we can perform any kind of arithmetic operations on numbers in python
+    
+    both int and float both used for number
+"""
+
+# Addition
+print(2 + 4) # 6
+
+# Subtraction
+print(2 - 4) # -2
+
+# Multiplication
+print(2 * 4) # 8
+
+# Division
+print(2 / 4) # 0.5
+
+# to the power of (Exponentials)
+print(2 ** 3) # 8
+
+# Interger division (double divide)
+print(5 // 4) # 1
+
+# Modulas (Remainder)
+print(3 % 2) # 1
+
+
+"""
+    type() : it return the type of the value
+"""
+
+print(type(4)) # <class 'int'>
+print(type(4.1)) # <class 'float'>
+
+"""
+    Python always choose higher precidence type for the resulting value
+"""
+
+# Ex: if we perform an addition between int and float the result will be float
+print(2 + 2.5) # 4.5
+
