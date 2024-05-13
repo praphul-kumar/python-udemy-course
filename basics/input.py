22d413dfeeacaa9815c12a67702be49443afad6b
Praphul <praphulkumar2023@gmail.com>
2024-05-13 13:04:41 +0530

    Taking Input in Python

---
+++ basics/input.py
@@ -0,0 +1,8 @@
+# Taking Input from user using console
+
+name = input("Enter Your Name: ")
+birth_year = int(input("Enter Birth Year: "))
+
+age = 2024 - birth_year
+
+print(f'hi, {name}, your are {age} years old.')
