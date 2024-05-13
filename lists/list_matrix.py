e282f72a06eab8493a9c058029c3760b47ba0676
Praphul Kumar <praphulkumar2023@gmail.com>
2024-05-13 22:44:35 +0530

    Matrix Intro

---
+++ lists/list_matrix.py
@@ -0,0 +1,17 @@
+# ------------- Matrix --------------
+
+"""
+    Matrix is a represantation of a Multi-Dimensional List
+"""
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+# We can access elements of matrix by following the below pattern
+
+print(matrix[0][1]) # 2
+print(matrix[1][0]) # 4
+print(matrix[2][2]) # 9
