625223d713507d21de3c12d851817fe568c74192
Praphul <praphulkumar2023@gmail.com>
2024-05-13 10:32:00 +0530

    Excape sequences

--- basics/strings.py
+++ basics/strings.py
@@ -37,3 +37,17 @@ c = type(b)
 print(c) # <class 'str'>
 
 
+# Escape Sequence in python
+
+"""
+    \t - Tab
+    \n - New line
+    \' - single quote
+    \" = double quote
+"""
+
+weather = "\t It\'s \"kind of\" sunny \n hope you have a good day!"
+
+print(weather)
+
+
