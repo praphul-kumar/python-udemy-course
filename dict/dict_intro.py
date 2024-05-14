3d1b001ecb268aa7b82d956038c9c15ed70cbead
Praphul Kumar <praphulkumar2023@gmail.com>
2024-05-14 15:38:06 +0530

    Dictionary Intro

---
+++ dict/dict_intro.py
@@ -0,0 +1,18 @@
+# Introduction to Dictionary
+
+"""
+    Dictionary:- 
+        - In Python, dictionaries are mutable data structures 
+        - that allow you to store key-value pairs.
+
+        - the Key in a dictionary must be an immutable data type 
+        - key must be unique in the dictionary, otherwise it the value will be overwriten 
+"""
+
+# Syntax:
+dict_1 = {
+    'key-1': 'value 1',
+    'key-2': 'value 2',
+    'key-3': 'value 3',
+    'key-4': 'value 4',
+}
