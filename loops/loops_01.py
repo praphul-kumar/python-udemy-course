a458bfbf274b86480cc7ce01a68ba7a3708f9cca
Praphul Kumar <praphulkumar2023@gmail.com>
2024-05-21 10:56:34 +0530

    Loop Intro

---
+++ loops/loops_01.py
@@ -0,0 +1,8 @@
+"""
+    Loops:-
+        - Python programming language provides two types of Python loopshecking time. 
+        - we will look at Python loops and understand their working with the help of examp 
+        – For loop and While loop to handle looping requirements. 
+        
+        - Loops in Python provides three ways for executing the loops.
+"""
