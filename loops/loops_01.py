ccf4d2f0c4f99861f14c5f470e5cd9aa2faad099
Praphul Kumar <praphulkumar2023@gmail.com>
2024-05-22 17:59:42 +0530

    Loops

--- loops/loops_01.py
+++ loops/loops_01.py
@@ -5,4 +5,8 @@
         – For loop and While loop to handle looping requirements. 
         
         - Loops in Python provides three ways for executing the loops.
+
+    Types of Loop in Python: 
+        1. While loop
+        2. for loop
 """
